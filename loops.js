39b48f4690e2235832dacdc5505945b56a875a54
vainqueur <vainqueurmg@gmail.com>
2025-05-28 09:32:15 +0200

    Add functions.js and loops.js files with basic loop implementations

---
+++ loops.js
@@ -0,0 +1,23 @@
+// while loop 
+
+let i = 0 ;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// do hile loop 
+
+let j = 0 ;
+
+do {
+    console.log(j);
+    j++;
+} while (j < 5);
+
+// for loop
+
+for (let k = 0; k < 5; k++) {  // three major parts : initialization , condition , execution 
+    console.log(k);
+}
+
